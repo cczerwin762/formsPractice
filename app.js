de757a6cf212c2a58005dabea831381081575eec
cczerwin762 <cczerwin@udel.edu>
2022-06-20 21:32:54 -0400

    added basic JS

---
+++ app.js
@@ -0,0 +1,14 @@
+function verify(){
+  p1 = document.getElementById('password').value;
+  p2 = document.getElementById('vpassword').value;
+  uname = document.getElementById('username').value;
+  console.log(p1+""+p2);
+  if(p1!=p2 || p1.length==0||uname.length===0){
+    alert("Passwords must match, and all fields must be filled out.");
+    return false;
+  }
+  else{
+    alert("Hello " + uname +"! Thank you for creating an account." )
+  }
+}
+console.log("Start \n");
